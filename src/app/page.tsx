eb6a2c79144fe2baeb4beee887d9b045a98db6bf
Noah Stevens <ss.noah@gmail.com>
2024-09-04 00:21:34 +1000

    Use trpc and vscode settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
 
 import { db } from "~/server/db";
-import { HydrateClient } from "~/trpc/server";
+import { api, HydrateClient } from "~/trpc/server";
+import { LatestPost } from "./_components/post";
 
 export default async function Home() {
   const posts = await db.query.posts.findMany();
+  void api.post.getLatest.prefetch();
   console.log(posts);
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-         {posts.map((post) => (
-            <div key={post.id}>
-              <h1>{post.name}</h1>
-              <p>{post.createdAt.toLocaleDateString()}</p>
-            </div>
-          ))}
+         <LatestPost></LatestPost>
         </div>
       </main>
     </HydrateClient>
