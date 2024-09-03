9224f0f94dc209b85bf152033375f6e66fc1b067
Noah Stevens <ss.noah@gmail.com>
2024-09-04 00:37:30 +1000

    fix trpx prefetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
-
-import { db } from "~/server/db";
+import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
-import { LatestPost } from "./_components/post";
 
 export default async function Home() {
-  const posts = await db.query.posts.findMany();
+  const hello = await api.post.hello({ text: "from tRPC" });
+
   void api.post.getLatest.prefetch();
-  console.log(posts);
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-         <LatestPost></LatestPost>
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-2xl text-white">
+              {hello ? hello.greeting : "Loading tRPC query..."}
+            </p>
+          </div>
+          <LatestPost />
         </div>
       </main>
     </HydrateClient>
