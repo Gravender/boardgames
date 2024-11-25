775c2829160648a57473ed980f19fe32027e917b
gravender <ss.noah@gmail.com>
2024-11-25 22:56:34 +1100

    tidy games page

--- src/app/dashboard/games/page.tsx
+++ src/app/dashboard/games/page.tsx
@@ -1,23 +1,15 @@
+"use server";
+
 import { auth } from "@clerk/nextjs/server";
-import { Plus } from "lucide-react";
-import { AddGameForm } from "~/app/_components/addGameForm";
+import { AddGameDialog } from "~/app/_components/addGameDialog";
 import { Games } from "~/app/_components/games";
-import { Button } from "~/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "~/components/ui/dialog";
-import { api } from "~/trpc/server";
+import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Page() {
   const { userId } = await auth();
   const games = userId ? await api.game.getGames() : [];
   return (
-    <Dialog>
+    <HydrateClient>
       <div>
         {userId ? (
           <Games games={games} />
@@ -25,22 +17,8 @@ export default async function Page() {
           <span>You need to be logged in to view this page.</span>
         )}
       </div>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Add Game</DialogTitle>
-        </DialogHeader>
-        <AddGameForm />
-      </DialogContent>
-      <div className="flex h-full w-full flex-col justify-between">
-        <div className="">Stuff</div>
-        <div className="flex justify-end p-4">
-          <DialogTrigger asChild>
-            <Button variant="default" className="rounded-full" size="icon">
-              <Plus />
-            </Button>
-          </DialogTrigger>
-        </div>
-      </div>
-    </Dialog>
+
+      <AddGameDialog />
+    </HydrateClient>
   );
 }
