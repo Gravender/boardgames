fb9de178935dedbc39219cacf0dbf09bf157188d
gravender <ss.noah@gmail.com>
2025-01-08 08:54:06 +1100

    refactor: :recycle: move insights and analytics to own components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import "~/styles/globals.css";
 
 import { type Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
+import { Analytics } from "~/components/analytics";
+import { SpeedInsights } from "~/components/speedInsights";
 import { ThemeProvider } from "~/components/theme-provider";
 import { Toaster } from "~/components/ui/toaster";
 import { TRPCReactProvider } from "~/trpc/react";
@@ -17,6 +17,14 @@ export const metadata: Metadata = {
   description: "Board Games Tracker created by gravender",
   creator: "@gravender",
   icons: [{ rel: "games", url: "/favicon.ico" }],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://boardgame.gravender.net/dashboard",
+    title: "Board Games Tracker",
+    description: "Board Games Tracker created by gravender",
+    siteName: "Board Games Tracker",
+  },
 };
 
 export default function RootLayout({
