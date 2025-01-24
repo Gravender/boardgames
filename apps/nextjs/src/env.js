68f2ce36c0e976fa7909874c73b0a48d3ec96f8d
gravender <ss.noah@gmail.com>
2025-01-25 06:08:16 +1100

    update next env

--- apps/nextjs/src/env.js
+++ apps/nextjs/src/env.js
@@ -1,4 +1,5 @@
 import { createEnv } from "@t3-oss/env-nextjs";
+import { vercel } from "@t3-oss/env-nextjs/presets";
 import { config } from "dotenv";
 import { z } from "zod";
 
@@ -9,12 +10,14 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars.
    */
-  server: {
-    POSTGRES_URL: z.string().url(),
-    VERCEL_URL: z.string().url().optional(),
+  extends: [vercel()],
+  shared: {
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+  },
+  server: {
+    POSTGRES_URL: z.string().url(),
   },
 
   /**
@@ -31,7 +34,6 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    VERCEL_URL: process.env.VERCEL_URL,
     POSTGRES_URL: process.env.POSTGRES_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
