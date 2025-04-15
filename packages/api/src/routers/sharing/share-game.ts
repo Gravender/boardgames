5fefe0d924128c9914b1e7e9a6372c4f978779ab
gravender <ss.noah@gmail.com>
2025-04-15 16:44:48 +1000

    feat(api): Add game sharing functionality and define game interface
    - Introduce a new router for sharing games, allowing users to retrieve shared game details and associated matches.
    - Implement error handling for cases where shared games are not found.
    - Define a comprehensive TypeScript interface for game and match data, enhancing type safety and clarity in the API.

---
+++ packages/api/src/routers/sharing/share-game.ts
@@ -0,0 +1,100 @@
+import { TRPCError } from "@trpc/server";
+
+import { selectSharedGameSchema } from "@board-games/db/zodSchema";
+
+import { createTRPCRouter, protectedUserProcedure } from "../../trpc";
+
+export const shareGameRouter = createTRPCRouter({
+  getSharedGame: protectedUserProcedure
+    .input(selectSharedGameSchema.pick({ id: true }))
+    .query(async ({ ctx, input }) => {
+      const returnedGame = await ctx.db.query.sharedGame.findFirst({
+        where: {
+          id: input.id,
+          sharedWithId: ctx.userId,
+        },
+        with: {
+          game: {
+            with: {
+              image: true,
+            },
+          },
+          sharedMatches: {
+            where: {
+              sharedWithId: ctx.userId,
+            },
+            with: {
+              match: {
+                with: {
+                  location: true,
+                },
+              },
+              sharedMatchPlayers: {
+                with: {
+                  sharedPlayer: {
+                    with: {
+                      linkedPlayer: true,
+                    },
+                  },
+                  matchPlayer: true,
+                },
+              },
+            },
+          },
+          linkedGame: {
+            with: {
+              image: true,
+            },
+          },
+        },
+      });
+      if (!returnedGame) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Shared game not found.",
+        });
+      }
+      const linkedGame = returnedGame.linkedGame;
+      return {
+        id: returnedGame.id,
+        name: linkedGame ? linkedGame.name : returnedGame.game.name,
+        imageUrl: linkedGame
+          ? linkedGame.image?.url
+          : returnedGame.game.image?.url,
+        permission: returnedGame.permission,
+        yearPublished: linkedGame
+          ? linkedGame.yearPublished
+          : returnedGame.game.yearPublished,
+        players: {
+          min: linkedGame
+            ? linkedGame.playersMin
+            : returnedGame.game.playersMin,
+          max: linkedGame
+            ? linkedGame.playersMax
+            : returnedGame.game.playersMax,
+        },
+        playtime: {
+          min: returnedGame.game.playtimeMin,
+          max: returnedGame.game.playtimeMax,
+        },
+        ownedBy: linkedGame ? linkedGame.ownedBy : returnedGame.game.ownedBy,
+        matches: returnedGame.sharedMatches.map((mMatch) => ({
+          type: "shared" as const,
+          id: mMatch.match.id,
+          gameId: returnedGame.id,
+          date: mMatch.match.date,
+          location: mMatch.match.location?.name,
+          finished: mMatch.match.finished,
+          name: mMatch.match.name,
+          duration: mMatch.match.duration,
+          won:
+            mMatch.sharedMatchPlayers.findIndex(
+              (sharedMatchPlayer) =>
+                sharedMatchPlayer.matchPlayer.winner &&
+                sharedMatchPlayer.sharedPlayer?.linkedPlayer?.userId ===
+                  ctx.userId,
+            ) !== -1,
+        })),
+      };
+    }),
+});
