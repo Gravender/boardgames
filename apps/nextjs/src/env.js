78520db17bc2064f82779e1fe5a75342f3873214
gravender <ss.noah@gmail.com>
2025-06-14 09:00:41 +1000

    refactor: update PostHog API host configuration and clean up environment variables

--- apps/nextjs/src/env.js
+++ apps/nextjs/src/env.js
@@ -30,7 +30,6 @@ export const env = createEnv({
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
     NEXT_PUBLIC_POSTHOG_KEY: z.string(),
-    NEXT_PUBLIC_POSTHOG_HOST: z.string(),
   },
 
   /**
@@ -42,7 +41,6 @@ export const env = createEnv({
     SIGNING_SECRET: process.env.SIGNING_SECRET,
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
-    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
