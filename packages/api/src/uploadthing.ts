f65bed502282263b2f3ec0eacd9e1f237e93d659
gravender <ss.noah@gmail.com>
2025-06-14 21:53:21 +1000

    feat: add match image handling with upload and retrieval functionality

--- packages/api/src/uploadthing.ts
+++ packages/api/src/uploadthing.ts
@@ -3,10 +3,10 @@ import { getAuth } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
 import { createUploadthing } from "uploadthing/next";
 import { UploadThingError, UTApi } from "uploadthing/server";
+import z from "zod/v4";
 
 import { db } from "@board-games/db/client";
-import { image, user } from "@board-games/db/schema";
-import { insertImageSchema } from "@board-games/db/zodSchema";
+import { image, matchImage, user } from "@board-games/db/schema";
 
 const f = createUploadthing();
 
@@ -21,7 +21,22 @@ export const uploadRouter = {
       maxFileCount: 1,
     },
   })
-    .input(insertImageSchema.pick({ usageType: true }))
+    .input(
+      z.discriminatedUnion("usageType", [
+        z.object({
+          usageType: z.literal("match"),
+          matchId: z.number(),
+          caption: z.string().optional(),
+          duration: z.number().optional(),
+        }),
+        z.object({
+          usageType: z.literal("game"),
+        }),
+        z.object({
+          usageType: z.literal("player"),
+        }),
+      ]),
+    )
     .middleware(async ({ req, input }) => {
       const authUser = getAuth(req);
       // eslint-disable-next-line @typescript-eslint/only-throw-error
@@ -34,7 +49,7 @@ export const uploadRouter = {
         // eslint-disable-next-line @typescript-eslint/only-throw-error
         throw new UploadThingError("Could not find user");
       }
-      return { userId: returnedUser.id, usageType: input.usageType };
+      return { userId: returnedUser.id, usageType: input.usageType, input };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       const [returnedImage] = await db
@@ -52,6 +67,22 @@ export const uploadRouter = {
       if (!returnedImage) {
         // eslint-disable-next-line @typescript-eslint/only-throw-error
         throw new UploadThingError("Image not added to database");
+      }
+      if (metadata.input.usageType === "match") {
+        const [returnedMatchImage] = await db
+          .insert(matchImage)
+          .values({
+            matchId: metadata.input.matchId,
+            imageId: returnedImage.id,
+            createdBy: metadata.userId,
+            caption: metadata.input.caption,
+            duration: metadata.input.duration,
+          })
+          .returning();
+        if (!returnedMatchImage) {
+          // eslint-disable-next-line @typescript-eslint/only-throw-error
+          throw new UploadThingError("MatchImage not added to database");
+        }
       }
       return { uploadedBy: metadata.userId, imageId: returnedImage.id };
     }),
