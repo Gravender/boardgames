a8f397c7ed56308de7e99eba7759a6d3eb577017
gravender <ss.noah@gmail.com>
2025-07-04 02:31:06 +1000

    feat: implement metadata generation for game stats page

--- apps/nextjs/src/app/dashboard/games/[id]/stats/page.tsx
+++ apps/nextjs/src/app/dashboard/games/[id]/stats/page.tsx
@@ -1,12 +1,30 @@
+import type { Metadata } from "next";
 import { Suspense } from "react";
 import { redirect } from "next/navigation";
 
-import { HydrateClient, prefetch, trpc } from "~/trpc/server";
+import { caller, HydrateClient, prefetch, trpc } from "~/trpc/server";
 import { StatsPageSkeleton } from "../../_components/game-stats-skeleton";
 import GameStats from "./_components/game-stats";
 
 interface Props {
   params: Promise<{ id: string }>;
+}
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // read route params
+  const id = (await params).id;
+
+  if (isNaN(Number(id))) redirect("/dashboard/games");
+  const game = await caller.game.getGameMetaData({ id: Number(id) });
+  if (!game) redirect("/dashboard/games");
+  if (!game.image?.url)
+    return { title: `${game.name} Stats`, description: `${game.name} Stats` };
+  return {
+    title: `${game.name} Stats`,
+    description: `${game.name} Stats`,
+    openGraph: {
+      images: [game.image.url],
+    },
+  };
 }
 export default async function GameStatsPage({ params }: Props) {
   const id = (await params).id;
