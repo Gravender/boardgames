5522d6abfec446a5064e5a1a032695205aec0d90
gravender <ss.noah@gmail.com>
2025-08-06 00:17:04 +1000

    fix:typecheck for api and auth for jsx

--- packages/auth/eslint.config.js
+++ packages/auth/eslint.config.js
@@ -1,11 +1,12 @@
 import baseConfig, { restrictEnvAccess } from "@board-games/eslint-config/base";
+import reactConfig from "@board-games/eslint-config/react";
 
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
-    ignores: [],
+    ignores: ["dist/**"],
   },
   ...baseConfig,
-
+  ...reactConfig,
   ...restrictEnvAccess,
 ];
