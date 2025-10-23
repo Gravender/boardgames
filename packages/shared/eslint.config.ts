7c0084def13e5e48b9714f6b81f034febf6d20d4
gravender <ss.noah@gmail.com>
2025-10-23 18:15:26 +1100

    fix shared eslint config

--- packages/shared/eslint.config.ts
+++ packages/shared/eslint.config.ts
@@ -1,12 +1,17 @@
+import type { Linter } from "eslint";
 import { defineConfig } from "eslint/config";
 
 import { baseConfig } from "@board-games/eslint-config/base";
 import { reactConfig } from "@board-games/eslint-config/react";
 
-export default defineConfig(
+const config = defineConfig(
   {
     ignores: ["dist/**"],
   },
   baseConfig,
   reactConfig,
-);
+) satisfies Linter.Config[];
+
+const typedConfig = config as Linter.Config[];
+
+export default typedConfig;
