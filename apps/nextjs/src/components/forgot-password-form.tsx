d33d11232307761711178a7a8da04143a3622563
gravender <ss.noah@gmail.com>
2025-11-03 14:14:52 +1100

    fix: standardize sign-up URL path across middleware and forms

--- apps/nextjs/src/components/forgot-password-form.tsx
+++ apps/nextjs/src/components/forgot-password-form.tsx
@@ -100,7 +100,7 @@ export function ForgotPasswordForm({
               </div>
               <div className="text-center text-sm">
                 Don&apos;t have an account?{" "}
-                <Link href="/signup" className="underline underline-offset-4">
+                <Link href="/sign-up" className="underline underline-offset-4">
                   Sign up
                 </Link>
               </div>
