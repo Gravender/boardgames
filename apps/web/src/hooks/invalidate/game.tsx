fd5a10d4cc1b3e18fdde19160125cf42d141487a
gravender <ss.noah@gmail.com>
2026-04-14 09:22:39 +1000

    refactor: update dependencies and remove unused components
    - Replaced `lodash.debounce` with `lucide-react` and added `next-themes` and `recharts` to dependencies for enhanced functionality.
    - Removed deprecated components related to match sharing and player statistics, streamlining the codebase.
    - Updated `uuid` dependency to a previous version for compatibility.
    - Cleaned up the `bun.lock` and `package.json` files to reflect the changes in dependencies and removed unnecessary devDependencies.

--- apps/web/src/hooks/invalidate/game.tsx
+++
@@ -1,9 +0,0 @@
-import { useCallback } from "react";
-import { useQueryClient } from "@tanstack/react-query";
-
-export function useInvalidateGames() {
-  const queryClient = useQueryClient();
-  return useCallback(() => {
-    return [queryClient.invalidateQueries()];
-  }, [queryClient]);
-}
