70001d7464e3b51b8ae6bf17f80dde55d91739e2
Noah Stevens <ss.noah@gmail.com>
2026-05-25 15:47:39 +1000

    remove test

--- apps/web/src/test/link.typed-routes.tsx
+++
@@ -1,6 +0,0 @@
-import { Link } from "~/components/link";
-
-export const invalidTypedRoute = (
-  // @ts-expect-error invalid typed route for compile-time enforcement
-  <Link href="/this-route-does-not-exist">Broken Link</Link>
-);
